7e56db71e3aaf413fac048d225741daf15a85332
teru <85771338+teruteru214@users.noreply.github.com>
2024-12-25 12:35:22 +0900

    Refactor: 本番用と開発用のcors設定や接続文字列などを設定する

--- Program.cs
+++ Program.cs
@@ -3,32 +3,48 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+var allowedOrigins = builder.Configuration["AllowedOrigins"]?.Split(',')
+    ?? Environment.GetEnvironmentVariable("AllowedOrigins")?.Split(',')
+    ?? new string[] { "http://localhost:5173" };
+
+if (builder.Environment.IsDevelopment())
+{
+    Console.WriteLine($"Allowed Origins (Development): {string.Join(", ", allowedOrigins)}");
+}
+else
+{
+    Console.WriteLine($"Allowed Origins (Production): {string.Join(", ", allowedOrigins)}");
+}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("MyCorsPolicy", builder =>
+    options.AddPolicy("MyCorsPolicy", corsBuilder =>
     {
-        builder.WithOrigins("https://green-island-06327a400.4.azurestaticapps.net")
-            .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-            .WithHeaders("Content-Type");
+        corsBuilder
+            .WithOrigins(allowedOrigins)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
     });
 });
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseRouting();
 app.UseCors("MyCorsPolicy");
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.MapControllers();
 
 app.Run();
